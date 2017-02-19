8cfe45ee7f22ac21137d066254d54e2af2daf79b
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-02-19 15:52:16 -0500

    Change status text when switch is clicked

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@ $(document).ready(function() {
   console.log("Document is ready!")
 
   $('.switch').on('click', function () {
-    console.log("Click registered.  ")
+    console.log("Click registered.");
+
+    $('.status').text('Hey, who turned off the lights?');
+
   });
 
 });
